0aa1cb6f3bf5abcafb9e9f126b63e7a93a631c42
Vladimir Kamuz <v.kamuz@gmail.com>
2019-12-10 23:54:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php get_header(); ?>
 	<div class="container">
 		<h1 class="title"><?php bloginfo('name') ?></h1>
-		<ul>
-		<?php if(have_posts()): while(have_posts()) : the_post(); ?>
-			<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
-		<?php endwhile; endif; ?>
-		</ul>
+		<?php if(have_posts()): ?>
+			<ul>
+				<?php while(have_posts()) : the_post(); ?>
+					<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
+				<?php endwhile;?>
+			</ul>
+		<?php endif; ?>
 	</div>
 <?php get_footer(); ?>
