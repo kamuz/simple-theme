bc0807ae7e19d89c29d8284f5d9499d8a4e4b879
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-09-10 23:17:25 +0300

    Add pagination

--- index.php
+++ index.php
@@ -2,11 +2,20 @@
 	<div class="container">
 		<h1 class="title"><?php bloginfo('name') ?></h1>
 		<?php if(have_posts()): ?>
-			<ul>
+			<ul id="posts-list">
 				<?php while(have_posts()) : the_post(); ?>
 					<li><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></li>
 				<?php endwhile;?>
 			</ul>
+			<div class="pagination">
+				<?php
+				global $wp_query;
+				echo paginate_links([
+					'total' => $wp_query->max_num_pages,
+					'current' => max(1, get_query_var('paged')),
+					'type' => 'list', ]);
+				?>
+			</div>
 		<?php endif; ?>
 	</div>
 <?php get_footer(); ?>
