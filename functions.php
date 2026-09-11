d52f57e7391b9a0e6e1731cc7a6a3416ca4ab762
Volodymyr Kamuz <v.kamuz@gmail.com>
2026-09-11 18:24:02 +0300

    Little fix

--- functions.php
+++ functions.php
@@ -24,7 +24,7 @@ add_action( 'after_setup_theme', 'simple_theme_setup' );
  * Register Scripts and Style
  */
 function theme_register_scripts() {
-	wp_enqueue_style( 'simple-css', get_stylesheet_uri() );
+	wp_enqueue_style( 'simple-css', get_stylesheet_uri(), false, '1.0.0' );
 	wp_enqueue_script( 'simple-js', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '1.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'theme_register_scripts', 1 );
